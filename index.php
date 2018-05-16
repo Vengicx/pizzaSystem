4221df8fd1e307324546f46330c72a8658e8d819
Vengicx <kitocx@gmail.com>
2018-05-16 01:05:22 -0300

    Teste de insert

--- index.php
+++ index.php
@@ -1,30 +1,36 @@
 <html>
-    <head>
-      <!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-      <link rel="stylesheet" href="css/style.css">
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      <title>Pizza Genérica</title>
-    </head>
-    <body>
-      <nav>
-        <div class="nav-wrapper">
-          <a href="#" class="brand-logo center">Logo</a>
-          <ul id="nav-mobile" class="left hide-on-med-and-down">
-            <li><a href="sass.html">Sass</a></li>
-            <li><a href="badges.html">Components</a></li>
-            <li><a href="collapsible.html">JavaScript</a></li>
-          </ul>
-        </div>
-      </nav>
-        
+  <head>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <link rel="stylesheet" href="css/style.css">
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Pizza Genérica</title>
+  </head>
+  <body>
+  <div class="container">
+    <h2 class="center">Cadastro de Matéria-Prima</h2>
+
+    <form action="save/materiaprima.php" method="post">
+      <div class="input-field col s6">
+        <input id="nomemateria" type="text" class="validate">
+        <label for="nomemateria">Nome da Matéria Prima</label>
+      </div>
+      <div class="input-field col s6">
+          <input id="valormateria" type="text" class="validate">
+          <label for="valormateria">Valor em R$</label>
+      </div>
+      <button class="btn waves-effect waves-light" type="submit" name="action">Enviar
+        <i class="material-icons right">send</i>
+      </button>
+    
+    </form>
 
 
-
-      <!--JavaScript at end of body for optimized loading-->
-      <script type="text/javascript" src="js/materialize.min.js"></script>
-    </body>
+  </div>
+  <!--JavaScript at end of body for optimized loading-->
+  <script type="text/javascript" src="js/materialize.min.js"></script>
+  </body>
   </html>
