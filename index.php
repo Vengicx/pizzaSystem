f147eb1716fb6c3f48bdb38ae56127f649b23df9
Vengicx <kitocx@gmail.com>
2018-05-23 16:39:11 -0300

    index, footer, menu

--- index.php
+++ index.php
@@ -10,21 +10,17 @@
       <title>Pizza Genérica</title>
     </head>
     <body>
-      <nav>
-        <div class="nav-wrapper">
-          <a href="#" class="brand-logo center">Logo</a>
-          <ul id="nav-mobile" class="left hide-on-med-and-down">
-            <li><a href="sass.html">Sass</a></li>
-            <li><a href="badges.html">Components</a></li>
-            <li><a href="collapsible.html">JavaScript</a></li>
-          </ul>
-        </div>
-      </nav>
-        
+    <nav>
+      <?php include "menu.php"; ?>
+    </nav>
+      <main>
+        <h1 class="center">Teste</h1>
 
-
-
-      <!--JavaScript at end of body for optimized loading-->
+      </main>
+    <footer class="page-footer">
+      <?php include "footer.php"; ?>
+    </footer>
+        <!--JavaScript at end of body for optimized loading-->
       <script type="text/javascript" src="js/materialize.min.js"></script>
     </body>
   </html>
---
+++ menu.php
@@ -0,0 +1,9 @@
+<div class="nav-wrapper indigo">
+    <a href="#" class="brand-logo center">Logo</a>
+        <ul id="nav-mobile" class="left hide-on-med-and-down">
+            <li><a href="sobre.php">Sobre</a></li>
+            <li><a href="sabores.php">Sabores</a></li>
+            <li><a href="contato.php">Contato</a></li>
+        </ul>
+</div>
+   
